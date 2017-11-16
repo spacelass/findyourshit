b17a90b54a8d590e73ef481899ad8dc07bd1a1ed
Henry Ke <hnryke@gmail.com>
2017-11-16 08:03:36 -0800

    added area controller and views

--- app/controllers/areas_controller.rb
+++ app/controllers/areas_controller.rb
@@ -1,4 +1,8 @@
 class AreasController < ApplicationController
+  def index
+    @areas = Area.all
+  end
   def show
+    @area = Area.find(params[:id])
   end
 end
--- app/models/area.rb
+++ app/models/area.rb
@@ -1,4 +1,4 @@
 class Area < ApplicationRecord
   has_many :buildings
-  has_many :bathrooms, :through :buildings
+  has_many :bathrooms, through: :buildings
 end
