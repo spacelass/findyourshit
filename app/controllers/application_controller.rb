2424cd75e9ba5af7510cf43c31016c97b03443f7
Colleen Yu <colleenyu98@berkeley.edu>
2017-11-16 16:00:49 -0800

    ok

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,6 @@ class ApplicationController < ActionController::Base
   protected
    def configure_permitted_parameters
      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :male, :female])
+     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :male, :female])
    end
 end
